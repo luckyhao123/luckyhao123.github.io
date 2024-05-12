b150fce69c6f7a11eea6ac7d99764eadc36fb4e8
lucky man <2499634418@qq.com>
2024-05-12 18:36:46 +0800

    Initial commit

---
+++ scripts/main.js
@@ -0,0 +1,12 @@
+let myHeading = document.querySelector("h1");
+myHeading.textContent="hello world";
+function setUserName() {
+    let myName = prompt("请输入你的名字。");
+    if (!myName) {
+      setUserName();
+    } else {
+      localStorage.setItem("name", myName);
+      myHeading.textContent = "Mozilla 酷毙了，" + myName;
+    }
+}
+  setUserName();  
